08c8fd531ae40a0657d647c381d74bc9a0da96b3
= <=>
2023-01-30 20:49:03 +0100

    account module added

---
+++ src/modules/account/presentation/pages/AccountArticles/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+function Account(): JSX.Element {
+
+    return(
+        <main className={styles.main}>
+
+        </main>
+    );
+}
+
+export default Account;
